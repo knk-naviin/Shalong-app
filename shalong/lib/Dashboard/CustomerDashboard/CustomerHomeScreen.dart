15cdd3487cd5b74a3f5fb975d944a90edf1f030a
knk-naviin <knknaveen28@gmail.com>
2021-10-03 11:05:15 +0530

    Ui Modified in Customer Screen

--- shalong/lib/Dashboard/CustomerDashboard/CustomerHomeScreen.dart
+++ shalong/lib/Dashboard/CustomerDashboard/CustomerHomeScreen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
@@ -18,7 +19,7 @@ class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
   List<String> favorites = [];
   // bool _icon = false;
   var searchText = "";
-
+  var name = FirebaseAuth.instance.currentUser!.displayName;
   RefreshController _refreshController =
       RefreshController(initialRefresh: false);
   TextEditingController editingController = TextEditingController();
@@ -55,83 +56,140 @@ class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
   Widget shopList(List<ShopInfo>? shops) {
     if (shops == null) {
       _onLoading();
-      return Center(child: Scaffold(
+      return Center(
+          child: Scaffold(
         backgroundColor: Colors.white70,
         body: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Platform.isAndroid?CircularProgressIndicator(
-
-                ):CupertinoActivityIndicator(
-                  animating: true,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text("Loading"),
-                )
-              ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Platform.isAndroid
+                ? CircularProgressIndicator()
+                : CupertinoActivityIndicator(
+                    animating: true,
+                  ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Loading"),
             )
-        ),
+          ],
+        )),
       ));
     }
     List<Widget> widgets = [];
-    widgets.add(
-     Column(
-       children: [
-         Container(
-           width: 440,
-           height: 200,
-           child: Stack(
-             children: [
-               new Container(
-                 width: 440,
-                 height: 180,
-                 decoration:  BoxDecoration(
-                     color: Colors.blue,
-                   borderRadius:  BorderRadius.only(
-                     bottomLeft: Radius.circular(36),
-                     bottomRight: Radius.circular(36),
-                   )
-                 ),
+    widgets.add(Column(
+      children: [
+        Container(
+          width: 440,
+          height: 200,
+          child: Stack(
+            children: [
+              new Container(
+                width: 440,
+                height: 180,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(36),
+                      bottomRight: Radius.circular(36),
+                    ),
+                    gradient: LinearGradient(
+                        colors: [Colors.lightBlueAccent, Colors.blue])),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Text(
+                        "Hi '",
+                        style: TextStyle(
+                          fontSize: 23,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Center(
+                      child: Text(
+                        name!,
+                        style: TextStyle(
+                            fontSize: 23,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20.0),
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    height: 54,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                              offset: Offset(0, 10),
+                              blurRadius: 50,
+                              color: Colors.blue.withOpacity(0.23))
+                        ]),
+                    child: TextField(
+                      onChanged: (value) {
+                        setState(() {});
+                      },
+                      controller: editingController,
+                      decoration: InputDecoration(
+                          // suffixIcon: Icon(Icons.search,color: Colors.blue,),
+                          hintText: "Search",
+                          hintStyle: TextStyle(color: Colors.blue),
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none),
+                    ),
+                  ))
+              // Padding(
+              //   padding: const EdgeInsets.only(top: 150.0),
+              //   child: TextField(
+              //
 
-               ),
-               Padding(
-                 padding: const EdgeInsets.only(top: 150.0),
-                 child: TextField(
-
-                   onChanged: (value) {
-                     setState(() {});
-                   },
-                   controller: editingController,
-                   decoration: InputDecoration(
-                     fillColor: Colors.red,
-                       labelText: "Search",
-                       hintText: "Search",
-                       prefixIcon: Icon(Icons.search),
-                       border: OutlineInputBorder(
-                           borderRadius: BorderRadius.all(Radius.circular(12.0)))),
-                 ),
-               ),
-             ],
-           ),
-         ),
-
-       ],
-     )
-
-
-    );
+              //
+              //     decoration: InputDecoration(
+              //       fillColor: Colors.red,
+              //         labelText: "Search",
+              //
+              //         prefixIcon: Icon(Icons.search),
+              //         border: OutlineInputBorder(
+              //             borderRadius: BorderRadius.all(Radius.circular(12.0)))),
+              //   ),
+              // ),
+            ],
+          ),
+        ),
+      ],
+    ));
     // for (var shop in shops) {
-      widgets.add(
-        Expanded(
-          child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: shops.length,
-              itemBuilder: (context, index) {
-                var shop = shops[index];
-                if (editingController.text.isEmpty) {
-                  return Card(
+    widgets.add(
+      Expanded(
+        child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: shops.length,
+            itemBuilder: (context, index) {
+              var shop = shops[index];
+              if (editingController.text.isEmpty) {
+                return Center(
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(40), // if you need this
+                      side: BorderSide(
+                        color: Colors.grey.withOpacity(0.2),
+                        width: 1,
+                      ),
+                    ),
                     borderOnForeground: true,
                     child: ListTile(
                       onTap: () {
@@ -192,7 +250,9 @@ class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
                                   favorites.contains(shop.docId)
                                       ? Icons.favorite
                                       : Icons.favorite_outline,
-                                  color: favorites.contains(shop.docId) ? Colors.blue : Colors.grey,
+                                  color: favorites.contains(shop.docId)
+                                      ? Colors.blue
+                                      : Colors.grey,
                                 ),
                                 onPressed: () {
                                   setState(() {
@@ -213,48 +273,62 @@ class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
                               color: shop.isOpen ? Colors.red : Colors.grey,
                               fontWeight: FontWeight.bold)),
                     ),
-                  );
-                } else if (shops[index]
-                        .name
-                        .toLowerCase()
-                        .contains(editingController.text) ||
-                    shops[index]
-                        .name
-                        .toLowerCase()
-                        .contains(editingController.text)) {
-                  return ListTile(
-                    onTap: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ShopPageScreen(
-                              shopname: shop.name,
-                              shopadd: shop.address,
-                              isopen: shop.isOpen,
-                              phoneno: shop.phone,
-                            )),
-                      );
-                    },
-                    title: Text(shop.name),
-                  );
-                } else {
-                  return Container();
-                }
-              }),
-        ),
-        /**/
-      );
-      //);
+                  ),
+                );
+              } else if (shops[index]
+                      .name
+                      .toLowerCase()
+                      .contains(editingController.text) ||
+                  shops[index]
+                      .name
+                      .toLowerCase()
+                      .contains(editingController.text)) {
+                return ListTile(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ShopPageScreen(
+                                shopname: shop.name,
+                                shopadd: shop.address,
+                                isopen: shop.isOpen,
+                                phoneno: shop.phone,
+                              )),
+                    );
+                  },
+                  title: Text(shop.name),
+                );
+              } else {
+                return Container();
+              }
+            }),
+      ),
+      /**/
+    );
+    //);
     // }
 
-    return Column(children: widgets,);
+    return Column(
+      children: widgets,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.lightBlueAccent, Colors.blue])),
+        ),
+        backgroundColor: Colors.blue,
         elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.widgets),
+          onPressed: () {},
+          color: Colors.white,
+        ),
       ),
       body: SmartRefresher(
         enablePullDown: true,
